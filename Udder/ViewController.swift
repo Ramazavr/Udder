55d71fe59e6c011362fd3a7b7bcbd3dd4975b6e9
Roman Eigert <ramazavr@gmail.com>
2020-04-23 18:07:51 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Роман on 23.04.2020.
+//  Copyright © 2020 Roman Eigert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
